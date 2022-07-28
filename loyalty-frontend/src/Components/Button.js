cd565520b1f837082f03eaa1fcfd7f2dbc2bd0c2
Shivansh Kapoor <kapoor.shivansh2002@gmail.com>
2022-07-28 16:02:01 +0530

    make it better

--- loyalty-frontend/src/Components/Button.js
+++ loyalty-frontend/src/Components/Button.js
@@ -1,32 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import QRCode from "react-qr-code";
-const Button=({objid})=>{
+const Button = ({ objid }) => {
+  const [qr, setqr] = useState("no");
+  const size = "150";
 
-const [qr,setqr]=useState('no')
+  const qrgen = () => {
+    setqr("yes");
+  };
 
-
-    const qrgen=()=>{
-        setqr('yes')
-    }
-
-if(qr==='no'){
-    return(
-        <React.Fragment>
-            <button onClick={qrgen}>Click Me</button>
-            
-        </React.Fragment>
+  if (qr === "no") {
+    return (
+      <React.Fragment>
+        <button onClick={qrgen}>Click Me</button>
+      </React.Fragment>
     );
-}
-if(qr==='yes'){
-    return(
-        <React.Fragment>
-            <button onClick={qrgen}>Click Me</button>
-            
-            
-        </React.Fragment>
+  }
+  if (qr === "yes") {
+    return (
+      <React.Fragment>
+        <div>
+          <button onClick={qrgen}>Click Me</button>
+        </div>
+        <QRCode value={objid} size={size === "" ? 0 : size} />
+      </React.Fragment>
     );
-}
-   
-}
+  }
+};
 
 export default Button;
