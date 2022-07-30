c4aaa129e016d5857c6240e18ee1659f2a96bc0e
Rishi Awasthi <rishi220202@gmail.com>
2022-07-31 02:01:30 +0530

    Inserted Data for Chart and CSS changes

--- loyalty-frontend/src/Components/Data.js
+++ loyalty-frontend/src/Components/Data.js
@@ -1,27 +1,42 @@
 export const UserData=[
     {
         id:1,
-        year:'January',
-        userGain:80000,
-        userLost:823,
+        year:'Electronics',
+        userGain:8000
     },
     {
         id:2,
-        year:'February',
-        userGain:45677,
-        userLost:401,   
+        year:'Fashion',
+        userGain:4567
     },
     {
         id:3,
-        year:'March',
-        userGain:18888,
-        userLost:555,   
+        year:'Appliances',
+        userGain:1888
     },
     {
         id:4,
-        year:'April',
-        userGain:60598,
-        userLost:456
+        year:'Books',
+        userGain:6059
+    },
+    {
+        id:5,
+        year:'Toys',
+        userGain:7023
+    },
+    {
+        id:6,
+        year:'Grocery',
+        userGain:2440
+    },
+    {
+        id:7,
+        year:'Echo and Alexa',
+        userGain:5673
+    },
+    {
+        id:8,
+        year:'Kindle E-Readers and E-Books',
+        userGain:9035
     }
-
 ]
