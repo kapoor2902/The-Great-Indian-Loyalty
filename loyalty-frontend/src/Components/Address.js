5c81c4753e0603aa3e2aa3a53a648677dda298b3
Shivansh Kapoor <72663505+kapoor2902@users.noreply.github.com>
2022-08-01 01:34:11 +0530

    Update Address.js

--- loyalty-frontend/src/Components/Address.js
+++ loyalty-frontend/src/Components/Address.js
@@ -26,7 +26,7 @@ function Address() {
     };
 
     axios
-      .get("https://api.positionstack.com/v1/forward", { params })
+      .get("http://api.positionstack.com/v1/forward", { params })
       .then((response) => {
         dispatch({
           type: "SET_ADDRESS",
